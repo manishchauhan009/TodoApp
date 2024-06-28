e0aeab427fade9ef4d3cb9a842f850755634a3ac
Chauhan Manish Umashankar <164005214+manishchauhan009@users.noreply.github.com>
2024-06-29 00:15:04 +0530

    Update todoRoutes.js

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todoControllers');
 
-router.post('/todos', todoController.createTodo);
+router.post('/createtodos', todoController.createTodo);
 router.get('/todos', todoController.getTodos);
 router.put('/todos/:id', todoController.updateTodo);
 router.delete('/todos/:id', todoController.deleteTodo);
