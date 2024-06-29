96239cb054d8c2c14ca43ec05b4dbd0e20c195ac
Chauhan Manish Umashankar <164005214+manishchauhan009@users.noreply.github.com>
2024-06-29 12:09:05 +0530

    Update todoRoutes.js

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todoControllers');
 
+router.get("/",async (req,res)=>{
+  res.send("Welcome to home route")
+})
 router.post('/createtodos', todoController.createTodo);
 router.get('/todos', todoController.getTodos);
 router.put('/todos/:id', todoController.updateTodo);
